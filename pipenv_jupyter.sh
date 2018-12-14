fc84d79d2f2f1cdac522765f919c6c5e0c97c4d9
txoof <aaron.ciuffo@gmail.com>
2018-12-14 16:15:28 +0100

    add cleanup

--- pipenv_jupyter.sh
+++ pipenv_jupyter.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 # Prepare a project to work with pipenv and jupyter
 #https://stackoverflow.com/questions/47295871/is-there-a-way-to-use-pipenv-with-jupyter-notebook
+add_kernel(){
+  #pipenv install ipykernel
+  venvDir=`pipenv --venv`
+  projectName=`basename $venvDir`
+  pipenv run python -m ipykernel install --user --name="${projectName}"
+  echo "ipython/jupyter kernelspec is installed for project $projectName"
+  echo "you must now launch jupyter notebook and choose the kernel: $projectName"
+  read -p "Launch jupyter notebook now? [N/y]" -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    jupyter notebook
+  fi
+}
+
+clean_kernel() {
+  venvDir=`pipenv --venv`
+  if [ $? -ne 0 ]; then
+    echo no virtual environtment found to clean - exiting
+    exit 0
+  fi
+  echo $venvDir
+}
 
 pipenv -h > /dev/null 2>&1
 if [ $? -eq 0 ]; then
@@ -12,9 +34,9 @@ else
 fi
 
 case "$1" in
-  -a|--add) add=1
+  -a|--add) add_kernel
             ;;
-  -c|--clean) clean=1
+  -c|--clean) clean_kernel
             ;;
   -h|*) scriptName=`basename $0`
       echo help:
@@ -26,15 +48,4 @@ case "$1" in
 esac
 
 
-#pipenv install ipykernel
-workingDir=`pwd`
-projectName=`basename $workingDir`
-pipenv run python -m ipykernel install --user --name="${projectName}"
-echo "ipython/jupyter kernelspec is installed for project $projectName"
-echo "you must now launch jupyter notebook and choose the kernel: $projectName"
-read -p "Launch jupyter notebook now? [N/y]" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  jupyter notebook
-fi
 
