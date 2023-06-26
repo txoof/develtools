35f7a07f798589f9c6d6acb58e2c8fa8dfba40a7
txoof <aaron.ciuffo@gmail.com>
2023-06-26 14:39:31 +0200

    remove need to specify version

--- pipenv_jupyter.sh
+++ pipenv_jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # created by Aaron Ciuffo https://github.com/txoof
-# version 0 15 December 2018
+# version 0.1 26 June 2023
 
 # Prepare a project to work with pipenv and jupyter by creating a pipvenv and adding
 # the kernelspec to jupyter's list of available kernels 
@@ -15,32 +15,23 @@ add_kernel(){
   # pipenv does not play nice with pyenv and somtimes chooses the wrong
   # python version -- this will force it to use the 'global' python
   python=$1
-  pyVersion=`$1 -V | tr '[:upper:]' '[:lower:]'` 
-#  if [[ -x $python ]]
-#  then
-#    echo "using $python to install setup base environment and install kernelspec"
-#  else
-#    echo "could not find an executable for $1; exiting"
-#    exit 0
-#  fi
 
-  echo "PYTHON VERSION: $pyVersion"
-  pipenv --$pyVersion install ipykernel --skip-lock
+  pipenv install ipykernel --skip-lock
   venvDir=`pipenv --venv`
   projectName=`basename $venvDir`
   pipenv run python -m ipykernel install --user --name="${projectName}"
   if [ $? -eq 0 ];
   then
     echo "ipython/jupyter kernelspec is installed for project $projectName"
-    echo "you must now launch jupyter notebook and choose the kernel: $projectName"
-    read -p "Launch jupyter notebook now? [N/y]" -n 1 -r
+    echo "you can now launch jupyter notebook and choose the kernel: $projectName"
+    # read -p "Launch jupyter notebook now? [N/y]" -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
       jupyter notebook
     fi
   else
     echo "failed to install project ipykernel."
-    echo "try running: $ pipenv install ipykernel"
+    echo "try running: '$ pipenv install ipykernel'"
     echo "and run this script again"
     exit 0
   fi
@@ -81,9 +72,7 @@ else
 fi
 
 case "$1" in
-  -2) add_kernel "python2"
-            ;;
-  -3) add_kernel "python3"
+  -i) add_kernel 
             ;;
   -c|--clean) clean_kernel
             ;;
@@ -91,9 +80,8 @@ case "$1" in
       scriptName=`basename $0`
       echo creates a virtual environment and adds a ipython/jupyter kernelspec
       echo help:
-      echo     usage: $scriptName "[-2|-3|-c|-h]"
-      echo '   -2,       create a Python 2 virtual environment and jupyter kernel for this project'
-      echo '   -3,       create a Python 3 virtual environment and jupyter kernel for this project'
+      echo     usage: $scriptName "[-i|-c|-h]"
+      echo '   -i,       create a virtual environment and jupyter kernel for this project'
       echo '   -c,       remove jupyter kernels and this virutal environment'
       echo '   -h        this help'
       ;;
